b5fe6e2c5c41efc40239a60eb71898bfb718c41e
Maksim Kozyrin <maksim.kozyrin789@mail.ru>
2022-10-18 20:38:23 +0700

    feat: добавлено тело трекера инициативы

--- src/app/master/initiative-tracker/initiative-tracker.component.ts
+++ src/app/master/initiative-tracker/initiative-tracker.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Unit } from 'src/app/shared/models/unit';
 
 @Component({
   selector: 'app-initiative-tracker',
   templateUrl: './initiative-tracker.component.html',
-  styleUrls: ['./initiative-tracker.component.scss']
+  styleUrls: ['./initiative-tracker.component.scss'],
 })
 export class InitiativeTrackerComponent implements OnInit {
+  units: Unit[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.units = [
+      { name: 'Варвар', initiative: '10', color: 'green' },
+      { name: 'Лучник', initiative: '20', color: 'green' },
+      { name: 'Воин союзник', initiative: '12', color: 'yellow' },
+      { name: 'Слизь', initiative: '3', color: 'red' },
+    ];
   }
-
 }
---
+++ src/app/shared/models/unit.ts
@@ -0,0 +1,5 @@
+export interface Unit {
+  name: string;
+  initiative: string;
+  color: string;
+}
