ec8560aa8a25329285f363d581db929a688e28e8
MaksiMilian789 <maksim.kozyrin789@mail.ru>
2022-10-19 13:29:26 +0700

    fix: исправлено отображение трекера на устройствах шириной менее 365px

--- src/app/master/initiative-tracker/initiative-tracker.component.ts
+++ src/app/master/initiative-tracker/initiative-tracker.component.ts
@@ -18,10 +18,10 @@ export class InitiativeTrackerComponent implements OnInit {
     this.units = [
       { name: 'Варвар', initiative: '10', color: 'green' },
       { name: 'Лучник', initiative: '20', color: 'green' },
-      { name: 'Воин союзник', initiative: '12', color: 'yellow' },
+      { name: 'Воин чемпион союзник', initiative: '12', color: 'yellow' },
       { name: 'Слизь', initiative: '3', color: 'red' },
       { name: 'Мара', initiative: '12', color: 'green' },
-      { name: 'Элейна', initiative: '13', color: 'green' },
+      { name: 'Элейна Лиадон', initiative: '13', color: 'green' },
       { name: 'Гоблин', initiative: '18', color: 'red' },
       { name: 'Алазар', initiative: '8', color: 'green' },
       { name: 'Маг союзник', initiative: '7', color: 'yellow' },
