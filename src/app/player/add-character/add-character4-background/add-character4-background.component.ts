7d6479cf1ca36f1c77985b5b40f31939d9706be8
Maksim Kozyrin <maksim.kozyrin789@mail.ru>
2022-11-16 02:10:33 +0700

    feat: заготовка четвёртого этапа создания персонажа

--- src/app/player/add-character/add-character4-background/add-character4-background.component.ts
+++ src/app/player/add-character/add-character4-background/add-character4-background.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-character4-background',
   templateUrl: './add-character4-background.component.html',
   styleUrls: ['./add-character4-background.component.scss']
 })
-export class AddCharacter4BackgroundComponent implements OnInit {
+export class AddCharacter4BackgroundComponent {
+  addForm: FormGroup;
 
-  constructor() { }
+  backgrounds = [
+    'Артист',
+    'Беспризорник',
+    'Благородный',
+    'Гильдейский ремесленник',
+    'Моряк',
+    'Мудрец',
+    'Народный герой',
+    'Отшельник',
+    'Преступник',
+    'Прислужник',
+    'Солдат',
+    'Чужеземец',
+    'Шарлатан',
+  ];
 
-  ngOnInit(): void {
+  ideologies = [
+    'Законный',
+    'Нейтральный',
+    'Злой',
+  ];
+
+  maxAge: number = -1;
+
+  constructor() {
+    this.addForm = new FormGroup({
+      background: new FormControl('', Validators.required),
+      ideology: new FormControl('', Validators.required),
+    });
+    //TODO: доделать распределение навыков
   }
 
+  save(): void {}
 }
