b1de9ea1e24dcfcd44a9ad3f1fb515b62fc0af00
Maksim Kozyrin <maksim.kozyrin789@mail.ru>
2024-04-30 17:41:55 +0700

    feat: расширена модель condition

--- src/app/core/models/condition.model.ts
+++ src/app/core/models/condition.model.ts
@@ -1,5 +1,12 @@
+import { System } from "@core/enums";
+import { Skill } from "./character/skills/skill.model";
+
 export interface Condition {
   id: number;
   name: string;
   description: string;
+  system: System;
+  authorId: number;
+  worldId?: number;
+  skillInstance: Skill[];
 }
