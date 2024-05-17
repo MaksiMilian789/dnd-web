7fa79cb20c87bc4db45f584379104db11cabd8d5
Maksim Kozyrin <maksim.kozyrin789@mail.ru>
2024-05-17 20:36:41 +0700

    feat: возможность добавить аватар персонажа

--- src/app/player/character/components/char-info-dialog/char-info-dialog.component.ts
+++ src/app/player/character/components/char-info-dialog/char-info-dialog.component.ts
@@ -1,9 +1,12 @@
+import { HttpEventType } from '@angular/common/http';
 import { Component, Inject } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { GENDER_LOCALIZATION, Gender, IDEOLOGY_LOCALIZATION, Ideology } from '@core/enums';
 import { Character } from '@core/models';
 import { Characteristics } from '@core/models/character/characteristics.model';
 import { CharacterService } from '@core/services/api/character.service';
+import { UploadService } from '@core/services/api/upload.service';
 import { TuiDialogContext } from '@taiga-ui/core';
 import { POLYMORPHEUS_CONTEXT } from '@tinkoff/ng-polymorpheus';
 
@@ -18,6 +21,7 @@ export interface CharInfoDialogData {
 })
 export class CharInfoDialogComponent {
   protected character: Character;
+  form: FormGroup;
 
   edit: boolean = false;
 
@@ -25,15 +29,28 @@ export class CharInfoDialogComponent {
   level = new FormControl(0, { nonNullable: true, validators: Validators.required });
   age = new FormControl(0, { nonNullable: true, validators: Validators.required });
 
+  loading: boolean = false;
+
+  img: FormControl = new FormControl();
+  readonly loadedFiles$ = this.img.valueChanges.subscribe((val) => {
+    this.uploadFile(val);
+  });
+
   constructor(
     @Inject(POLYMORPHEUS_CONTEXT)
     protected readonly context: TuiDialogContext<boolean, CharInfoDialogData>,
     private _characterService: CharacterService,
+    private _uploadService: UploadService,
+    private _snackbar: MatSnackBar,
   ) {
     this.character = context.data.character;
     this.name.setValue(this.character.name);
     this.level.setValue(this.character.level);
     this.age.setValue(this.character.age);
+
+    this.form = new FormGroup({
+      imageId: new FormControl(this.character.imageId),
+    });
   }
 
   close(): void {
@@ -46,10 +63,43 @@ export class CharInfoDialogComponent {
 
   save(): void {
     this._characterService
-      .editCharacterInfo(this.character.id!, this.name.value, this.level.value, this.age.value)
+      .editCharacterInfo(this.character.id!, this.name.value, this.level.value, this.age.value,  this.form.controls['imageId'].value)
       .subscribe(() => {
         this.context.completeWith(true);
       });
+  }
+
+  imageUrl(): string {
+    return this._uploadService.url(this.character.imageId ?? 0);
+  }
+
+  removeFile(): void {
+    this.form.controls['imageId'].setValue(null);
+  }
+
+  hasImage(): boolean {
+    return this.form.controls['imageId'].value;
+  }
+
+  removeImage(): void {
+    this.form.patchValue({ imageId: null });
+    this.img.reset();
+    this.loading = false;
+  }
+
+  private uploadFile(file: File): any {
+    if (file.size >= 10 * 1024 * 1024) {
+      this._snackbar.open('Превышен максимальный размер файла');
+      return;
+    }
+    this.loading = true;
+    this._uploadService.upload(file).subscribe((event) => {
+      if (event.type === HttpEventType.Response) {
+        this._snackbar.open('Изображение загружено');
+        this.form.patchValue({ imageId: event.body });
+        this.loading = false;
+      }
+    });
   }
 
   get localizeGender(): string {
@@ -58,5 +108,9 @@ export class CharInfoDialogComponent {
 
   get localizeIdeology(): string {
     return IDEOLOGY_LOCALIZATION[this.character.ideology];
+  }
+  
+  get image(): string | undefined {
+    return `${this.imageUrl()}`;
   }
 }
