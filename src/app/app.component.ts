21caacd2570b78482360784cd76134ed98b31c24
Maksim Kozyrin <maksim.kozyrin789@mail.ru>
2024-05-20 12:29:37 +0700

    feat: показ сообщения об обновлении

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SwUpdate } from '@angular/service-worker';
 import { LoadingBarService } from '@ngx-loading-bar/core';
-import { Observable, delay, tap } from 'rxjs';
+import { Observable, delay } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,18 @@ import { Observable, delay, tap } from 'rxjs';
 export class AppComponent {
   loaderValue$: Observable<number>;
 
-  constructor(public loader: LoadingBarService) {
+  constructor(
+    public loader: LoadingBarService,
+    private _updates: SwUpdate,
+    private _snackbar: MatSnackBar,
+  ) {
     this.loaderValue$ = loader.value$.pipe(delay(0));
+
+    _updates.versionUpdates.subscribe(() => {
+      _snackbar.open('Вышла новая версия приложения. Обновление произойдёт через 5 секунд!');
+      _updates.activateUpdate().then(() => {
+        document.location.reload();
+      });
+    });
   }
 }
