519ea21e54cae4ed8a6f2e85b7d362a09e741f53
Maksim Kozyrin <maksim.kozyrin789@mail.ru>
2024-05-20 15:38:16 +0700

    feat: добавлено версионирование

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SwUpdate } from '@angular/service-worker';
 import { LoadingBarService } from '@ngx-loading-bar/core';
+import { LOCAL_STORAGE } from '@ng-web-apis/common';
 import { Observable, delay } from 'rxjs';
+
+import { APP_CONFIG, AppConfig } from '@core/config';
 
 @Component({
   selector: 'app-root',
@@ -11,18 +14,21 @@ import { Observable, delay } from 'rxjs';
 })
 export class AppComponent {
   loaderValue$: Observable<number>;
-  reload: boolean = false;
 
   constructor(
     public loader: LoadingBarService,
     private _updates: SwUpdate,
     private _snackbar: MatSnackBar,
+    @Inject(APP_CONFIG) config: AppConfig,
+    @Inject(LOCAL_STORAGE) private readonly _localStorage: Storage,
   ) {
     this.loaderValue$ = loader.value$.pipe(delay(0));
 
     _updates.versionUpdates.subscribe(() => {
-      if (!this.reload) {
+      let version = _localStorage.getItem('version');
+      if (version != config.version) {
         _snackbar.open('Вышла новая версия приложения. Обновите страницу для корректной работы!');
+        _localStorage.setItem('version', config.version);
       }
     });
   }
